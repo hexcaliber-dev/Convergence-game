494cd17c73cb6d4ef3dd3105a301a1d57fe0b25e
Ben Cuan <github@bencuan.me>
2020-03-23 12:14:40 -0700

    Working terminal input

---
+++ Assets/Scripts/Terminal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+// Terminal User Input
+// When they click enter, text gets logged and
+// can be interpreted
+public class Terminal : MonoBehaviour {
+
+    public TMP_InputField input;
+
+    // Start is called before the first frame update
+    void Start () {
+        input.onSubmit.AddListener (delegate { SubmitCommand (); });
+    }
+
+    // Update is called once per frame
+    void Update () { }
+
+    private void SubmitCommand () {
+        string cmd = input.text;
+        input.text = "";
+        Debug.Log (cmd);
+        input.Select ();
+        EventSystem.current.SetSelectedGameObject (input.gameObject, null);
+        input.OnPointerClick (new PointerEventData (EventSystem.current));
+    }
+}
