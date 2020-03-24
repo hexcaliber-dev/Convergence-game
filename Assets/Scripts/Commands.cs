0c36de9397f7c908068a25aaa10e53385c29e426
Epicrider <omarepic1@gmail.com>
2020-03-23 21:00:05 -0700

    Add Commands.cs and implemented ls function

---
+++ Assets/Scripts/Commands.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+public class Commands : MonoBehaviour
+{
+    public TextAsset allFiles;
+    public TextAsset userAvailableFiles;
+
+
+    // For testing purposes
+    void Start()
+    {
+        ls();
+    }
+
+    public List<string> ls()
+    {
+        string read_text = userAvailableFiles.text;
+        List<string> file_list = read_text.Split('\n').ToList<string>(); // Split using Newline
+        return file_list;
+    }
+
+
+}
