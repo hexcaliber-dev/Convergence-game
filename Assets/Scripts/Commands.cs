39d4795002dccde0d5e7ee5b432dc6a84912a76c
Epicrider <omarepic1@gmail.com>
2020-03-24 10:29:34 -0700

    Define help function command

--- Assets/Scripts/Commands.cs
+++ Assets/Scripts/Commands.cs
@@ -8,7 +8,9 @@ using System.Linq;
 public class Commands : MonoBehaviour
 {
     public TextAsset allFiles;
+    public TextAsset allCommands;
     public TextAsset userAvailableFiles;
+    public TextAsset userAvailableCommands;
 
 
     // For testing purposes
@@ -19,10 +21,21 @@ public class Commands : MonoBehaviour
 
     public List<string> ls()
     {
-        string read_text = userAvailableFiles.text;
-        List<string> file_list = read_text.Split('\n').ToList<string>(); // Split using Newline
+        List<string> file_list = ReadToList(userAvailableFiles);
         return file_list;
     }
 
+    public List<string> help()
+    {
+        List<string> command_list = ReadToList(userAvailableCommands);
+        return command_list;
+    }
+
+    private List<string> ReadToList(TextAsset ta)
+    {
+        string read_text = ta.text;
+        List<string> read_list = read_text.Split('\n').ToList<string>(); // Split using Newline
+        return read_list;
+    }
 
 }
