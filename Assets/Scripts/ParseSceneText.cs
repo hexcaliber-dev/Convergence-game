30b99c38c94a2f619b4426fff20d8d6fcab95105
Eric Qian <eric@enumc.com>
2020-03-23 21:05:26 -0700

    add: text parser

---
+++ Assets/Scripts/ParseSceneText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParseSceneText : MonoBehaviour
+{
+    string textFileDir = "sceneText.txt";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    string parseSceneTextCtx(int selectNum) {
+        string rawDialog = File.ReadAllText(textFileDir);
+
+        char delim = ';';
+        string[] dialogVals = rawDialog.Split(delim);
+
+        if (selectNum < dialogVals.Length) {
+            return dialogVals[selectNum];
+        }
+        else {
+            return "invalid";
+        }
+
+    }
+}
