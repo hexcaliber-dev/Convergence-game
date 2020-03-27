016621812b35eeaeb60b28bad2f76b22f46a36ad
Ben Cuan <github@bencuan.me>
2020-03-26 22:16:06 -0700

    barebones hackablecamera

---
+++ Assets/Scripts/Hacking/HackableCamera.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class HackableCamera : HackableObject {
+
+    // Start is called before the first frame update
+    void Start () {
+        panelNo = 1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
