3dda73ee02775286929d1aed307a8e0d87a5f984
Ben Cuan <github@bencuan.me>
2020-03-29 11:08:42 -0700

    PoweredObject interface

---
+++ Assets/Scripts/Hacking/Generator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : HackableObject {
+
+    // Powered objects matched with their ID's
+    public Dictionary<int, PoweredObject> connectedObjs;
+
+    void Start() {
+        panelNo = 2;
+    }
+
+    // Sets the power state of object ID to POWER_STATE.
+    void SetPower(int id, bool powerState) {
+        connectedObjs[id].SetPower(powerState);
+    }
+}
