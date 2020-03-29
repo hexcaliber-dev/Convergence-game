580b3f8c8d6ad0c2b306b52e37be1ebc5eef46ee
Epicrider <omarepic1@gmail.com>
2020-03-29 10:40:17 -0700

    Add Door Script

---
+++ Assets/Scripts/Hacking/Door.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public Animator anim;
+    [SerializeField] int charge_req;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    
+
+}
