e399449f69a62581c33279271ea6fe9690045e76
Ben Cuan <github@bencuan.me>
2020-04-02 11:06:16 -0700

    Fix doors

--- Assets/Scripts/Hacking/Door.cs
+++ Assets/Scripts/Hacking/Door.cs
@@ -2,24 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : PoweredObject
-{
+public class Door : PoweredObject {
 
     public Animator anim;
     public bool openInitially;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-        reset();
+    void Start () {
+        anim = GetComponent<Animator> ();
+        reset ();
     }
 
-    public override void PowerOnAction(){ OpenDoor();}
-    public override void PowerOffAction() { CloseDoor(); }
+    public override void PowerOnAction () { OpenDoor (); }
+    public override void PowerOffAction () { CloseDoor (); }
 
-    public void OpenDoor(){ anim.SetTrigger("Open");}
-    public void CloseDoor(){ anim.SetTrigger("Close"); }
-    public void reset(){ }
+    public void OpenDoor () {
+        anim.ResetTrigger ("Close");
+        anim.SetTrigger ("Open");
+    }
+    public void CloseDoor () {
+        anim.ResetTrigger("Open");
+        anim.SetTrigger ("Close");
+    }
+    public void reset () { }
 
 }
